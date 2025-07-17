5378a6e3c238d7e81e275a67e69ecc3b0bd8f90b
AlbaraaBasha <21670310075@bartin.edu.tr>
2025-07-17 23:33:54 +0300

    add CustomButton widget and integrate it into OnBoardingViewBody

--- lib/features/onBoarding/presentation/views/widgets/on_boarding_view_body.dart
+++ lib/features/onBoarding/presentation/views/widgets/on_boarding_view_body.dart
@@ -1,6 +1,7 @@
 import 'package:dots_indicator/dots_indicator.dart';
 import 'package:flutter/material.dart';
 import 'package:fruit_hub/core/utils/app_colors.dart';
+import 'package:fruit_hub/core/widgets/custom_button.dart';
 import 'package:fruit_hub/features/onBoarding/presentation/views/widgets/on_boarding_page_view.dart';
 
 class OnBoardingViewBody extends StatelessWidget {
@@ -18,6 +19,12 @@ class OnBoardingViewBody extends StatelessWidget {
             activeColor: AppColors.primaryColor,
           ),
         ),
+        SizedBox(height: 26),
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 16),
+          child: CustomButton(onPressed: () {}, text: 'ابدأ الآن'),
+        ),
+        SizedBox(height: 43),
       ],
     );
   }
